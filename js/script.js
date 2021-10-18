07a0dce3b0754dd9a7d59969a253ba466f60571b
keyan bawazir <key42pc@gmail.com>
2021-10-18 08:15:13 +0300

    correct code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-// var number1 = int(document.getElementById('number1').value);
-// var number2 = int(document.getElementById('number2').value);
 document.getElementById('btn').onclick = function(){
-    var number1 = int(document.getElementById('number1').value);
-    var number2 = int(document.getElementById('number2').value);
-    if(document.getElementById('operation').options[0].value="+"){
-        var result = str(number1 + number2);
+    var number1 = parseInt(document.getElementById('number1').value);
+    var number2 = parseInt(document.getElementById('number2').value);
+    var operation = document.getElementById('operation').value;
+    // var result = 0;
+    // if (document.getElementById('operation').value=="+") {
+    if (operation == "+") {
+        var result = (number1 + number2).toString();
         document.getElementById('result').innerText = result;
-    }else if(document.getElementById('operation').options[2].value="*"){
-        var result = str(number1 * number2);
+    // } else if (document.getElementById('operation').value=="-") {
+               
+    } else if (operation == "-") {
+        result = (number1 - number2).toString();
         document.getElementById('result').innerText = result;
-    }else if(document.getElementById('operation').options[3].value="/"){
-        var result = str(number1 / number2);
+    // } else if (document.getElementById('operation').value=="*") {
+    } else if (operation == "*") {   
+        result = (number1 * number2).toString();
+        document.getElementById('result').innerText = result;
+    // } else if (document.getElementById('operation').value=="/") {
+    } else if (operation == "/") {    
+        result = (number1 / number2).toString();
         document.getElementById('result').innerText = result;
     }
 }
-    // if(document.getElementById('operation').options[0].value="+"){
-    //     if(document.getElementById('operation').options[0].text = document.getElementById('operation').options[0].value){
-    //         result = number1 + number2;
-    //         document.getElementById('result').innerText = result;
-    // }elseif(document.getElementById('operation').options[1].text = document.getElementById('operation').options[1].value){
-    //         result = number1 - number2;
-    //         document.getElementById('result').innerText = result;
-    // }elseif(document.getElementById('operation').options[2].text = document.getElementById('operation').options[2].value){
-    //         result = number1 * number2;
-    //         document.getElementById('result').innerText = result;
-    // }elseif(document.getElementById('operation').options[3].value= document.getElementById('operation').options[3].value){
-    //         result = number1 / number2;
-    //         document.getElementById('result').innerText = result;
-    // }
-   
-
+// document.getElementById('btn').onclick = function(){
+//     var number1 = parseInt(document.getElementById('number1').value);
+//     var number2 = parseInt(document.getElementById('number2').value);
+//     // var operation = document.getElementById('operation').value;
+//     // var result = 0;
+//     // if (document.getElementById('operation').value=="+") {
+//     if (document.getElementById('operation').options[0].value == "+") {
+//         var result = (number1 + number2).toString();
+//         document.getElementById('result').innerText = result;
+//     // } else if (document.getElementById('operation').value=="-") {
+               
+//     } else if (document.getElementById('operation').options[1].value == "-") {
+//         result = (number1 - number2).toString();
+//         document.getElementById('result').innerText = result;
+//     // } else if (document.getElementById('operation').value=="*") {
+//     } else if (document.getElementById('operation').options[2].value == "*") {   
+//         result = (number1 * number2).toString();
+//         document.getElementById('result').innerText = result;
+//     // } else if (document.getElementById('operation').value=="/") {
+//     } else if (document.getElementById('operation').options[3].value == "/") {    
+//         result = (number1 / number2).toString();
+//         document.getElementById('result').innerText = result;
+//     }
+// }
+    
